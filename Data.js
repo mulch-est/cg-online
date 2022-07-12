5b9119ffd95041f44136b9491079151587d9bd05
mulch <52766118+mulch-est@users.noreply.github.com>
2022-07-12 18:05:51 +0000

    finished up server side organization and stripping

--- Data.js
+++ Data.js
@@ -4,11 +4,13 @@ module.exports = class SvData {
     this.connected_sockets = [];
   }
 
+  //param socket contains the socket class, which is primarily used for its id member var
   connectSocket(socket){
     connected_sockets.push(socket);
   }
 
-  disconnectSocket(socket){
+  //param socket contains socket class, param hard is a bool: whether to remove socket from connected_sockets
+  disconnectSocket(socket, hard){
     //iterate through every player in every room to find correct socket
     let delNum = -1;
     for(let i=0; i<rooms.length; i++){
@@ -31,18 +33,96 @@ module.exports = class SvData {
     }
     //remove room from list if there are no players left
     if(delNum != -1)rooms.splice(delNum, 1);
+
+    //remove socket from connected_sockets if hard disconnect
+    if(hard){
+      let delSoc = -1;
+      for(let i=0; i<connected_sockets.length; i++){
+        if(connected_sockets[i].id === socket.id){
+          
+        }
+      }
+      if(delSoc != -1)connected_sockets.splice(delSoc, 1);
+    }
   }
+
+  //param data contains keys sent by client
+  moveClientRoom(data){
+    //iterate through every room
+    for(let i=0; i<rooms.length; i++){
+      //will cause glitches with multiple rooms of same name
+      if(rooms[i].name === data.room && data.prevRoom === "none"){
+        //add client to correct room and increment current players
+        rooms[i].players.push(data.client);
+        rooms[i].current++;
+      }
+    }
+  }
+
+  //generates valid room name, param rmname should be string & desired name
+  generateRoomName(rmname){
+    let nameOk=true;
+    if(rmname === "createRoom" || rmname === "playTutorial" || isEmpty(rmname)){
+      nameOk=false;
+    }else {
+      for(let i=0; i<rooms.length; i++){
+        if(rooms[i].name === rmname){
+          nameOk=false;
+          break;
+        }
+      }
+    }
+    if(nameOk){
+      return rmname;
+    }else {
+      //generate name since prompt answer was not valid
+      let attempt = makeid(8);
+      return generateRoomName(attempt);
+    }
+  }
+
+  //param data contains keys sent by client
+  requestRoom(data){
+    if(data.prevRoom==="none" && !isEmpty(data.room)){
+      //create room with name data.room (or a random name if that one is unavailable)
+      let roomName = generateRoomName(data.room);
+
+      let createdRoom = new Room(roomName, 2);
+      /* InitRoom()
+      loadCards(createdRoom);
+      createdRoom.deck=shuffle(createdRoom.deck);*/
+
+      //add data.client to the room
+      createdRoom.players.push(data.client);
+      createdRoom.current++;
+      createdRoom.index = rooms.length;
+      rooms.push(createdRoom);
+    }
+  }
+  
+}
+
+//creates a random string from var characters of length determined by int param length
+function makeid(length) {
+  let result= '';
+  let characters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function isEmpty(str) {
+  return (!str || 0 === str.length);
 }
 
 class Room {
   constructor(name, max){
-    this.name=name;
-    this.current=0;
-    this.max=max;
-    this.players=[];
-    this.number=rooms.length;
-    this.deck=[];
-    this.loadedMerch=false;
-    this.lastTurnEnd=0;
+    this.name = name;
+    this.current = 0;
+    this.max = max;
+    this.players = [];
+    this.index = -1;
   }
 }
--- index.js
+++ index.js
@@ -20,150 +20,35 @@ console.log((new Date())+" Server started");
 //create server class that houses room and socket data
 myServer = new SvData();
 
+function timestamp(){
+  date = new Date();
+  return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
 //precedes sockets code
 var io = require('socket.io')(serv,{});
 
 //all comms below
 io.sockets.on('connection', function(socket){
   myServer.connectSocket(socket);
-	console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Cl("+socket.id+") connected");
+	console.log(timestamp()+" Cl("+socket.id+") connected");
   //room list is emitted here if rooms are implemented
   
 	socket.on('disconnect',function (){
-    myServer.disconnectSocket(socket);
-    console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Cl("+socket.id+") disconnected");
+    myServer.disconnectSocket(socket, true);
+    console.log(timestamp()+" Cl("+socket.id+") disconnected");
 	});
 
-  socket.on('enterRoom', function(data){//enterRoom exp. increases in calls, unknown y
-    //console.log("adding 1 to current"+rooms.length);
-    for(let i=0; i<rooms.length; i++){
-      if(rooms[i].name===data.room && data.prevRoom==="none"){//will cause glitches with multiple rooms of same name
-        console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Cl("+data.client+") entered Rm("+data.room+")");
-        rooms[i].players.push(data.client);
-        rooms[i].current++;
-        io.sockets.emit('roomEdit', {
-          to:"all", 
-          name:rooms[i].name, 
-          current:rooms[i].current, 
-          max:rooms[i].max, 
-          players:rooms[i].players, 
-          number:rooms[i].number,
-        });
-      }
-    }
-
+  socket.on('enterRoom', function(data){//enterRoom exp. increases in calls, unknown why
+    myServer.moveClientRoom(data);
   });
 
   socket.on('createRoom', function(data){
-    if(data.prevRoom==="none" && !isEmpty(data.room)){
-      //create room with name data.room (or a random name if that one is unavailable)
-      let roomName = generateRoomName(data.room);
-      console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Cl("+data.client+") created Rm("+roomName+")");
-      let createdRoom = new Room(roomName, 2);
-      loadCards(createdRoom);
-      createdRoom.deck=shuffle(createdRoom.deck);
-      console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Cl("+data.client+") entered Rm("+roomName+")");
-      //add data.client to the room
-      createdRoom.players.push(data.client);
-      createdRoom.current++;
-      rooms.push(createdRoom);
-      //update room lists
-        io.sockets.emit('roomList', {
-          to:"all", 
-          name:createdRoom.name, 
-          current:createdRoom.current, 
-          max:createdRoom.max, 
-          players:createdRoom.players, 
-          number:createdRoom.number,
-        });
-        io.sockets.emit('roomReady', {
-          to:data.client, 
-          name:createdRoom.name, 
-          current:createdRoom.current, 
-          max:createdRoom.max, 
-          players:createdRoom.players, 
-          number:createdRoom.number,
-        });
-    }
+    myServer.requestRoom(data);
   });
 
-  function generateRoomName(rmname){
-    let nameOk=true;
-    if(rmname==="createRoom" || rmname==="playTutorial" || isEmpty(rmname)){
-      nameOk=false;
-    }else {
-      for(let i=0; i<rooms.length; i++){
-        if(rooms[i].name===rmname){
-          nameOk=false;
-          break;
-        }
-      }
-    }
-    if(nameOk===true){
-      return rmname;
-    }else {
-      //generate name since prompt answer was not valid
-      let attempt = makeid(8);
-      return generateRoomName(attempt);
-    }
-  }
-
-  function makeid(length) {
-    let result= '';
-    let characters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
-    let charactersLength = characters.length;
-    for (let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
-
-  function isEmpty(str) {
-    return (!str || 0 === str.length);
-  }
-
   socket.on('leaveRoom', function(data){
-    console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Cl("+data.client+") finished a game");
-    let delNum=-1;
-    for(let i=0; i<rooms.length; i++){
-      if(rooms[i].players.includes(data.client)){
-        let leftPlayer;
-        for(ii=0; ii<rooms[i].players.length; ii++){
-          if(rooms[i].players[ii]===data.client){
-            leftPlayer=ii;
-            rooms[i].players.splice(ii, 1);
-            break;
-          }
-        }
-
-        rooms[i].current--;
-
-        console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Rm("+rooms[i].name+") absolved Cl("+data.client+")");
-        if(!(rooms[i].current>=1 || rooms[i].name==="Funhouse"))console.log((new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds())+" Unlisted Rm("+rooms[i].name+")");
-
-        if(rooms[i].current>=1 || rooms[i].name==="Funhouse"){
-          io.sockets.emit('roomEdit', {
-            to:"all", 
-            name:rooms[i].name, 
-            current:rooms[i].current, 
-            max:rooms[i].max, 
-            players:rooms[i].players, 
-            number:rooms[i].number,
-            hasJaime:rooms[i].hasJaime,
-          });
-        }else {
-          io.sockets.emit('roomUnlist', {
-            to:"all", 
-            name:rooms[i].name, 
-            current:rooms[i].current, 
-            max:rooms[i].max, 
-            players:rooms[i].players, 
-            number:rooms[i].number,
-          });
-          delNum=i;
-        }
-      }
-    }
-    if(delNum!=-1)rooms.splice(delNum, 1);
+    console.log(timestamp()+" Cl("+data.client+") finished a game");
+    myServer.disconnectSocket(socket, false);
   });
 });
